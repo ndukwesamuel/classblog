c3d14516da420c3aa040a0f19811679693940ce7
ndukwesamuel <ndukwesamuel23@yahoo.com>
2021-05-01 11:33:58 +0100

    we thann gkod

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -38,8 +38,46 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
     'dash',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_REDIRECT_URL ="/accounts/login/"
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_UNIQUE =True
+
+
+if DEBUG:
+    EMAIL_BACKEND ='django.core.mail.backends.dummy.EmailBackend'
+
+else:
+    EMAIL_BACKEND ='django.core.mail.backends.dummy.EmailBackend'
+    # EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+
+
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        'APP': {
+            'client_id': '123',
+            'secret': '456',
+            'key': ''
+        }
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -70,6 +108,18 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+
+AUTHENTICATION_BACKENDS = [
+   
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+ 
 ]
 
 WSGI_APPLICATION = 'blogpost.wsgi.application'
