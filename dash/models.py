d24b3f331f8a2bc61ca24f5df810bf8a797e3429
ndukwesamuel <ndukwesamuel23@yahoo.com>
2021-05-01 11:55:20 +0100

    message added

--- dash/models.py
+++ dash/models.py
@@ -1,15 +1,20 @@
 from django.contrib.auth.models import  User
 from django.db import models
 from django.urls import reverse
+from  datetime import datetime, date
 
 # Create your models here.
 class post(models.Model):
     title =models.CharField(max_length=200)
     aurthor = models.ForeignKey(User,on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+   
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         # return reverse('Blogdetail', args=(str(self.id))) 
         return reverse('home') 
+
+
